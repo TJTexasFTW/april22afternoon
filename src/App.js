c059773d4883055395705af86e2076a4b1e9fef0
Theresa Stocking <nottodayjose@MacBook-Pro.attlocal.net>
2019-04-22 21:22:50 -0500

    Darn Button Centered

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
+  class App extends Component {
+    constructor() {
+      super();
+      this.state = {
+        menuStatus: ''
+      }
+    }
+    
+    handleClick = () => {
+      if(this.state.menuStatus === 'open') {
+        this.setState({
+          menuStatus: 'closed'
+        })
+      } else {
+        this.setState({
+          menuStatus: 'open'
+        })
+      }
+    }
+
+    render() {
+      return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <nav>
+      <h1>Start Bootstrap</h1>
+        <ul>
+          <li>SERVICES</li>
+          <li><a>PORTFOLIO</a></li>
+          <li><a>ABOUT</a></li>
+          <li><a>TEAM</a></li>
+          <li><a>CONTACT</a></li>
+          <li><a></a></li>
+
+        </ul>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQFRypROy2g5WEeVVPF7fImQKSxMQwd44t_3fA1yQROCKLEdFirA" alt="Hamburger Menu"/>
+    </nav>
+
+<body>
+    <div className="mobileMenu-">
+      <p>SERVICES</p>
+      <p>PORTFOLIO</p>
+      <p>ABOUT</p>
+      <p>TEAM</p>
+      <p>CONTACT</p>
     </div>
+
+<div className="Welcome">
+    <h2>Welcome To Our Studio!</h2>
+    <h1>IT'S NICE TO MEET YOU</h1>
+</div>
+        <div >
+              <button className="TellMe">TELL ME MORE</button>
+        </div>
+
+
+</body
+></div>
+
   );
+}
 }
 
 export default App;
---
+++ src/Components/services.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+
+{/* //SERVICES */}
+<div className="Services">
+  <h3>SERVICES</h3>
+  <h6>Lorem ipsum dolo sit amet consectetur</h6>
+
+</div>
